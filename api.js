094458632204a17a2451a69a5c1338aad9a9f36c
anubanls <113908787+anubanls@users.noreply.github.com>
2022-09-20 21:26:09 +0700

    Update api.js

--- api.js
+++ api.js
@@ -1,6 +1,7 @@
 //Url to Google Sheets
 //const url = 'https://docs.google.com/spreadsheets/d/[sheet ID]/gviz/tq?';
-const url = 'https://docs.google.com/spreadsheets/d/1qqcewCVPu3hTQTlgLBHbPWKJShlVnEiKu4ZdHLctPvc/gviz/tq?';
+// const url = 'https://docs.google.com/spreadsheets/d/1qqcewCVPu3hTQTlgLBHbPWKJShlVnEiKu4ZdHLctPvc/gviz/tq?';
+const url = 'https://docs.google.com/spreadsheets/d/103ej9yCCcGr_to-3MNbsZ9t6qgojuKMpDIaPet6dGkE/gviz/tq?';
 const productsContainer = document.querySelector('.products-grid');
     fetch(url)
     .then(res => res.text())
